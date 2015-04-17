3edc18ec0eb56978379113fef12d9d3d30110bbf
Sawyer Anderson <sawyer.lemon@gmail.com>
2015-04-17 18:06:39 -0500

    Actually writing dataset as csv

--- src/galileo/create_dataset.py
+++ src/galileo/create_dataset.py
@@ -25,20 +25,20 @@ def get_average_exchange_rate (start_time):
 	file_num = (start_time - first_trade_day) / seconds_in_day
 	read_next_file = True
 	seek = True
+	read_days = 0
 
-        read_days = 0
 	while (read_next_file):
-                if (read_days > 2):
-                    return -1
+		if (read_days > 2):
+			return -1
 		try:
 			with open(split_filepath_format + "_" + str(file_num) + ".csv") as csvfile:
-			        #print("filenum: " + str(file_num))
+				#print("filenum: " + str(file_num))
 				reader = csv.reader(csvfile)
-                                read_days += 1
+				read_days += 1
 				for line in reader:
 					trade_time = int(line[0])
 					if ((trade_time >= start_time) & (trade_time <= start_time + time_period_seconds)):
-                                        	seek = False
+						seek = False
 						total += float(line[1])
 						count += 1
 					else:
@@ -55,19 +55,18 @@ def get_average_exchange_rate (start_time):
 
 with open(comments_filepath, 'rb') as comments_file:
 	with open(output_filepath, 'a') as output_file:
+		writer = csv.writer(output_file, delimiter=",")
 		comments_reader = csv.reader(comments_file)
 		i = 0
 		for line in comments_reader:
-                        if(i % 1000 == 0):
-                                print(str(i) + " comments read.")
+			if(i % 1000 == 0):
+				print(str(i) + " comments read.")
 			time = int(line[1]) / 1000 #comments time is in ms, not s
 			average = get_average_exchange_rate(time)
-                        i += 1
-                        if (average == -1):
-                            continue
-                        line.append(str(average))
-			output_file.write(", ".join(line))
-                        output_file.write("\n")
+			i += 1
+			if (average == -1):
+				continue
+			writer.writerow([line[0], time, line[2], average])
 
 
 

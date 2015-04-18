07a039ed2985267261a8b683342440d471ee9022
Sawyer Anderson <sawyer.lemon@gmail.com>
2015-04-17 22:09:15 -0500

    Added difference option to create_dataset.py

--- src/galileo/create_dataset.py
+++ src/galileo/create_dataset.py
@@ -53,6 +53,43 @@ def get_average_exchange_rate (start_time):
 			return -1
 	return total / count
 
+#returns the difference between the price at start and the average over the subsequent time period
+def get_average_difference_exchange_rate(start_time):
+	total = 0.0
+	count = 0
+	file_num = (start_time - first_trade_day) / seconds_in_day
+	read_next_file = True
+	seek = True
+	read_days = 0
+	start_price = 0
+	while (read_next_file):
+		if (read_days > 2):
+			return -1
+		try:
+			with open(split_filepath_format + "_" + str(file_num) + ".csv") as csvfile:
+				#print("filenum: " + str(file_num))
+				reader = csv.reader(csvfile)
+				read_days += 1
+				for line in reader:
+					trade_time = int(line[0])
+					if ((trade_time >= start_time) & (trade_time <= start_time + time_period_seconds)):
+						if(seek):
+							start_price = float(line[1])
+						seek = False
+						total += float(line[1])
+						count += 1
+					else:
+						if(seek): #first row in first file probably isn't in range
+							continue
+						else:
+							read_next_file = False
+							break #think this is the right logic
+				file_num += 1
+		except IOError as e:
+			read_next_file = False
+			return -1
+	return (total / count) - start_price
+
 with open(comments_filepath, 'rb') as comments_file:
 	with open(output_filepath, 'a') as output_file:
 		writer = csv.writer(output_file, delimiter=",")
@@ -62,7 +99,8 @@ with open(comments_filepath, 'rb') as comments_file:
 			if(i % 1000 == 0):
 				print(str(i) + " comments read.")
 			time = int(line[1]) / 1000 #comments time is in ms, not s
-			average = get_average_exchange_rate(time)
+			#average = get_average_exchange_rate(time)
+			average = get_average_difference_exchange_rate(time)
 			i += 1
 			if (average == -1):
 				continue
